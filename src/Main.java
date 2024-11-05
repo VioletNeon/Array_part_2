14387b4f7b958d3810ec9e49f6f379a4eaba9c94
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-11-05 09:34:39 +0300

    Task-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        short[] weeklyPaymentAmounts = {15_000, 17_000, 19_000, 12_000, 23_000};
+        int sum = 0;
+
+        for (short item : weeklyPaymentAmounts) {
+            sum += item;
+        }
+
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
     }
 }
